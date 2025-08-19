ffb72ad5630bde06cb8ac0315de255c1395ed0ae
Jose Luis Riquelme <jose.riquelmes@usach.cl>
2025-08-19 12:40:41 -0400

    Creating note item composable

--- app/src/main/java/cl/onm/onenoteman/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/cl/onm/onenoteman/feature_note/presentation/notes/components/NoteItem.kt
@@ -1,22 +1,42 @@
 package cl.onm.onenoteman.feature_note.presentation.notes.components
 
 import android.R
+import android.R.attr.height
+import android.R.attr.maxLines
+import android.R.style
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 import cl.onm.onenoteman.feature_note.domain.model.Note
-import cl.onm.onenoteman.feature_note.domain.util.NoteOrder.Color
 
 @Composable
 fun NoteItem(
     note: Note,
-    modifier: Modifier= Modifier,
+    modifier: Modifier = Modifier,
     cornerRadius: Dp = 10.dp,
     cutCornerSize: Dp = 30.dp,
     onDeleteClick: () -> Unit
@@ -34,11 +54,53 @@ fun NoteItem(
             }
             clipPath(clipPath) {
                 drawRoundRect(
-                    color =Color(note.color),
-                    size = size
+                    color = Color(note.color),
+                    size = size,
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+
+                )
+                drawRoundRect(
+                    color = Color(ColorUtils.blendARGB(note.color, 0x000000, 0.2f)),
+                    topLeft = Offset(size.width - cutCornerSize.toPx(), 0f),
+                    size = Size(
+                        width = cutCornerSize.toPx() + 100f,
+                        height = cutCornerSize.toPx() + 100f
+                    ),
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
 
                 )
             }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 32.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = note.content,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 10,
+                overflow = Ellipsis
+            )
+        }
+        IconButton(
+            onDeleteClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete note",
+            )
         }
     }
 }
